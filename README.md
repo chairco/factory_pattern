b8410c2c939b911cdfd402dfea0535d205b8965a
chairco <chairco@gmail.com>
2016-08-26 23:45:04 +0800

    fix pandas read xlsx function

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # factory_pattern
+嘗試使用 Factory Pattern 來處理 xlsx 資料。
+流程主要為讀入、轉成 data set、然後再給負責處理資料 format 的 api 接手。
+ 
--- nb_method.py
+++ nb_method.py
@@ -1,27 +1,24 @@
-import json
-import openpyxl
 import pandas as pd
 
 
 class XLSconnector:
-
     def __init__(self, filepath):
-        self.data = dict()
-        with open(filepath, mode='r', encoding='utf-8') as f:
-            self.data = pd.read_csv(f)
+        self.data = pd.read_excel(filepath)
+        #self.data = dict()
+        #with open(filepath, mode='r', encoding='utf-8') as f:
+        #    self.data = pd.read_excel(f)
 
-
-    @property
+    @property    
     def parsed_data(self):
         return self.data
 
 
 def connection_factory(filepath):
     if filepath.endswith('xlsx'):
-        connection = XLSconnector
+        connector = XLSconnector
     else:
         raise ValueError('Cannot connect to {}'.format(filepath))
-    return connection(filepath)
+    return connector(filepath)
 
 
 def connect_to(filepath):
@@ -29,12 +26,14 @@ def connect_to(filepath):
     try:
         factory = connection_factory(filepath)
     except Exception as e:
-        print(e)
+        print('error:', e)
     return factory
 
 
 def main():
-    pass
+    xlsx_factory = connect_to('test.xlsx')
+    xlsx_data = xlsx_factory.parsed_data
+    print(xlsx_data)
 
 
 if __name__ == '__main__':
